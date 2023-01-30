7ed82ca311cf38c3fd6b12aebb7f7c605528e19d
Jacob Sebastian <jacobsebastian1729@gmail.com>
2023-01-30 17:14:48 +0100

    basic footer

--- project_management/src/components/footer.tsx
+++ project_management/src/components/footer.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+import "./style/footer.css";
+
 const footer = () => {
-  return <div>footer</div>;
+  return (
+    <div>
+      <footer className="footer">
+        <div className="footer_container">
+          <p className="footer_para">
+            &copy; Copyright © {new Date().getFullYear()} Company Name. All
+            rights reserved
+          </p>
+          <p className="footer_para">
+            <a href="/privacy-policy.html">Privacy Policy</a> |&nbsp; 
+            <a href="/terms-of-use.html">Terms of Use.</a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default footer;
